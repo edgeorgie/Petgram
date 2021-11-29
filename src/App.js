0f3b66f8d5409b0c8de6fe502d8b571e567dd3ee
Edwin Jorge <ed.jorge1122@gmail.com>
2021-11-29 14:24:11 -0500

    logo added

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import React from 'react'
 import { ListOfCategories } from './components/ListOfCategories'
 import { GlobalStyle } from './components/GlobalStyles'
 import { ListOfPhotoCards } from './components/ListOfPhotoCards'
+import { Logo } from './components/Logo'
 
 export const App = () => {
   return (
     <>
+      <Logo />
       <GlobalStyle />
       <ListOfCategories />
       <ListOfPhotoCards />
