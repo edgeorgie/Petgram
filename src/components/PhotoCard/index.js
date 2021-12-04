ae094d7308d307f827a73077b956d0224af4a727
Edwin Jorge <ed.jorge1122@gmail.com>
2021-12-03 19:10:13 -0500

    custom hooks

--- src/components/PhotoCard/index.js
+++ src/components/PhotoCard/index.js
@@ -1,43 +1,33 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React from 'react'
 import { Article, Button, Img, ImgWrapper } from './styles'
-import { MdFavoriteBorder } from 'react-icons/md'
+import { MdFavoriteBorder, MdFavorite } from 'react-icons/md'
+import { useLocalStorage } from '../../hooks/useLocalStorage'
+import { useNearScreen } from '../../hooks/useNearScreen'
 
 const DEFAULT_IMAGE = 'https://images.unsplash.com/photo-1447684808650-354ae64db5b8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
 
 export const PhotoCard = ({ id, likes = 0, src = DEFAULT_IMAGE }) => {
-  const element = useRef(null)
-  const [show, setShow] = useState(false)
+  const [show, element] = useNearScreen()
 
-  useEffect(() => {
-    import('intersection-observer')
-      .then(() => {
-        const observer = new window.IntersectionObserver((entries) => {
-          const { isIntersecting } = entries[0]
-          if (isIntersecting) {
-            console.log('Intersecting')
-            setShow(true)
-            observer.disconnect()
-          }
-        })
-        observer.observe(element.current)
-      })
-  }, [element])
+  const key = `like-${id}`
+  const [liked, setLiked] = useLocalStorage(key, false)
+
+  const Icon = liked ? MdFavorite : MdFavoriteBorder
 
   return (
     <Article ref={element}>
-      {
-        show &&
-          <>
-            <a href={`/detail/${id}`}>
-              <ImgWrapper>
-                <Img src={src} alt='Photo' />
-              </ImgWrapper>
-            </a>
-            <Button>
-              <MdFavoriteBorder size='32px' /> {likes} likes
-            </Button>
-          </>
-      }
+      {show && (
+        <>
+          <a href={`/detail/${id}`}>
+            <ImgWrapper>
+              <Img src={src} alt='Photo' />
+            </ImgWrapper>
+          </a>
+          <Button onClick={() => setLiked(!liked)}>
+            <Icon size='32px' /> {likes} likes
+          </Button>
+        </>
+      )}
     </Article>
   )
 }
